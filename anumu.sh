cb37388947a1bf479c10ace18f1814d5d83b407c
FdlyMots <131168637+FdlyMots@users.noreply.github.com>
2023-09-28 06:47:40 +0700

    Update anumu.sh

--- anumu.sh
+++ anumu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/FdlyMots/not2/main/setup.sh && chmod +x setup.sh && sed -i -e 's/\r$//' setup.sh && screen -S setup ./setup.sh￼Enter
-
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip 
+wget https://raw.githubusercontent.com/FdlyMots/not2/main/setup.sh && chmod +x setup.sh && sed -i -e 's/\r$//' setup.sh && screen -S setup ./setup.sh￼
 rm -rf anumu.sh
